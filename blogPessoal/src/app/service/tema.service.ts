7e15de81d306e80dd397bf8076fdfde683e4ecba
AndrehW27 <andrehwillian@hotmail.com>
2020-10-08 11:03:41 -0300

    implementação put e delete

--- blogPessoal/src/app/service/tema.service.ts
+++ blogPessoal/src/app/service/tema.service.ts
@@ -13,16 +13,25 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
   }
 
-  getAllTemas(){
+  getAllTemas() {
     return this.http.get('http://localhost:8080/tema', this.token)
   }
 
-  getByIdTema(id: number){
+  getByIdTema(id: number) {
     return this.http.get(`http://localhost:8080/tema/${id}`, this.token)
   }
 
-  postTema(tema: Tema){
+  postTema(tema: Tema) {
     return this.http.post('http://localhost:8080/tema', tema, this.token)
   }
+
+  putTema(tema: Tema) {
+    return this.http.put('http://localhost:8080/tema/', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
 
 }
